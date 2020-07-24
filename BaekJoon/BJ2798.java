f3aba2804666851c867a0cd2f3bfc309f5fdca04
TaeSeongMin <goji7309@gmail.com>
2020-07-24 17:23:28 +0900

    Add files via upload

---
+++ BaekJoon/BJ2798.java
@@ -0,0 +1,54 @@
+package com.ssafy.algo;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BJ2798 {
+	static int answer = 0;
+	static int M, N;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		int arr[] = new int[N];
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+			map.put(arr[i], i);
+		}
+
+		for (int i = 0; i < N; i++) {
+			boolean visited[] = new boolean[N];
+
+			dfs(visited, arr, i, 0, 0);
+		}
+		System.out.println(answer);
+
+	}
+
+	private static void dfs(boolean[] visited, int[] arr, int i, int cnt, int sum) {
+		if (cnt == 3) {
+			if (sum <= M) {
+				answer = Math.max(sum, answer);
+				
+			}
+			return;
+		}
+		cnt += 1;
+		for (int j = i; j < N; j++) {
+			if (visited[j])
+				continue;
+			visited[j] = true;
+			dfs(visited, arr, j, cnt, sum +arr[j]);
+			visited[j] = false;
+		}
+
+	}
+
+}
