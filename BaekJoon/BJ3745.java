40b3b235fb0e174fbcc682b9694d7c347b63537b
TaeSeongMin <mts7309@naver.com>
2020-10-09 18:58:35 +0900

    백준 유기농배추

--- BaekJoon/BJ3745.java
+++ BaekJoon/BJ3745.java
@@ -1,34 +1,38 @@
-package BaekJun;
+package BackJun;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 public class BJ3745 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String line = null;
-		while ((line = br.readLine()) != null) {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
 
-			int N = Integer.parseInt(line.trim());
-			int arr[] = new int[N];
+        while (sc.hasNext()) {
+            int N = sc.nextInt();
+            int arr[] = new int[N];
+            int dp[] = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int size= 0;
+            for (int i = 0; i < N; i++) {
+                int temp = Arrays.binarySearch(dp, 0, size, arr[i]); // 리턴값 : -insertPoint -1
+                temp = Math.abs(temp) - 1;//삽입위치
+                if (temp < 0) {
+                    dp[0] = arr[i];
+                }else
+                    dp[temp] = arr[i];// temp 자리에 값을 update 하면 그 의미는
+                // 0인덱스 위치부터 temp위치까지의 원소 갯수가  temp위치에 저장된 그 값을 마지막으로 하는 LIS 길이가 됨
+                // 배열의 원소가 LIS를 이루는 구성요소와는 관계가 없다.
 
-			StringTokenizer st = new StringTokenizer(br.readLine().trim());
-			for (int j = 0; j < N; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
-			}
-			bw.write(String.valueOf(arr.length));
-			bw.newLine();
+                if (size == temp) {// 삽입위치의 인덱스와 크기가 같으면(결국,마지막이 삽입위치라는 얘기임) 크기 1늘림.
+                    size++;
+                }
+            }
+            System.out.println(size);
+        }
 
-		}
-		bw.flush();
-		bw.close();
-
-	}
+    }
 }
