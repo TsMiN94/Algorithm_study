dbfa435e8de54b72af4421f10567197664a5d004
TaeSeongMin <mts7309@naver.com>
2020-10-09 20:47:51 +0900

    백준 피보나치함수

--- BaekJoon/BJ1003.java
+++ BaekJoon/BJ1003.java
@@ -1,42 +1,51 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+package BackJun;
 
-public class Main {
-	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	private static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-	private static int n, zero_cnt = 0, one_cnt = 0;
+import java.util.Scanner;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		n = Integer.parseInt(reader.readLine());
-		int arr[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(reader.readLine());
+public class BJ1003 {
+    static Fibo dp[] = new Fibo[41];
+    static int target;
+    static int zero = 0, one = 0;
 
-		}
-		for (int i = 0; i < n; i++) {
-			fibo(arr[i]);
-			System.out.println(zero_cnt + " " + one_cnt);
-			zero_cnt =0;
-			one_cnt =0;
-		}
-	}
-	
-	
+    public static void main(String[] args) {
+        dp[0] = new Fibo(0, 1, 0);
+        dp[1] = new Fibo(1, 0, 1);
+        dp[2] = new Fibo(2, 1, 1);
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            target = sc.nextInt();
+            fibo(target);
+            System.out.println(dp[target].zero + " " + dp[target].one);
+        }
+    }
 
-	public static int fibo(int n) {
-		if (n == 0) {
-			//System.out.println("0");
-			zero_cnt++;
-			return 0;
-		} else if (n == 1) {
-			//System.out.println("1");
-			one_cnt++;
-			return 1;
-		} else {
-			return fibo(n - 1) + fibo(n - 2);
-		}
-	}
+    private static Fibo fibo(int target) {
+        if (target == 0) {
+            return dp[0];
+        }
+        if (target == 1) {
+            return dp[1];
+        }
+        if (target == 2) {
+            return dp[2];
+        }
+        if (dp[target] == null) {
+            Fibo t_2 = fibo(target - 2);
+            Fibo t_1 = fibo(target - 1);
+            dp[target] = new Fibo(target, t_2.zero + t_1.zero, t_2.one + t_1.one);
+            return dp[target];
+        } else
+            return dp[target];
+    }
+
+    public static class Fibo {
+        int no, zero, one;
+
+        public Fibo(int no, int zero, int one) {
+            this.no = no;
+            this.zero = zero;
+            this.one = one;
+        }
+    }
 }
