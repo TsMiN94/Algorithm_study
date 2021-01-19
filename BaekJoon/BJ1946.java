b190ddd45097d5230b684d532b396d9af2a1981b
TaeSeongMin <mts7309@naver.com>
2021-01-19 23:33:49 +0900

    백준 신입사원

---
+++ BaekJoon/BJ1946.java
@@ -0,0 +1,17 @@
+package BackJun;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class BJ1946 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int answer = 0;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
